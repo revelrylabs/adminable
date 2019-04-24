8d60b06079b54576b9e039b8163b561720d0fabe
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-04-23 20:34:06 -0500

    Getting the schemas from the defimpls

--- lib/adminable/plug.ex
+++ lib/adminable/plug.ex
@@ -20,10 +20,27 @@ defmodule Adminable.Plug do
   end
 
   def call(conn, opts) do
+    repo = Keyword.fetch!(opts, :repo)
+    otp_app = Keyword.fetch!(opts, :otp_app)
+    layout = Keyword.get(opts, :layout, {Adminable.LayoutView, "app.html"})
+
+    path = :code.lib_dir(:app_template, :ebin)
+    schemas = Protocol.extract_impls(Adminable, [path])
+
+    schemas =
+      Enum.map(schemas, fn schema ->
+        {
+          schema.__schema__(:source),
+          schema
+        }
+      end)
+      |> Enum.into(%{})
+
     conn
-    |> Plug.Conn.assign(:repo, Keyword.fetch!(opts, :repo))
-    |> Plug.Conn.assign(:schemas, Keyword.get(opts, :schemas, %{}))
-    |> Plug.Conn.assign(:layout, Keyword.get(opts, :layout, {Adminable.LayoutView, "app.html"}))
+    |> Plug.Conn.assign(:otp_app, otp_app)
+    |> Plug.Conn.assign(:repo, repo)
+    |> Plug.Conn.assign(:schemas, schemas)
+    |> Plug.Conn.assign(:layout, layout)
     |> Adminable.Router.call(opts)
   end
 end
